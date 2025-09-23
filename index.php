1698fe1a2f3953cd76d22654a0082a176221dd99
root <dl3el@darc.de>
2025-09-23 11:49:23 +0200

    include link to github
    Signed-off-by: root <dl3el@darc.de>

--- index.php
+++ index.php
@@ -132,7 +132,11 @@ if ((defined('DL3EL_SC_CHANGE')) && (DL3EL_SC_CHANGE === "yes")) {
 <span style = "font-size: 32px;letter-spacing:4px;font-family: &quot;sans-serif&quot;, sans-serif;font-weight:500;color:PaleBlue"><?php echo $callsign;?></span>
 <p style = "margin-top:0px;margin-bottom:0px;">
 <span style = "font-size: 18px;letter-spacing:4px;font-family: &quot;sans-serif&quot;, sans-serif;font-weight:500;color:PaleBlue"><?php echo $fmnetwork; ?></span>
-<span style = "font-size: 12px;letter-spacing:2px;font-family: &quot;sans-serif&quot;, sans-serif;font-weight:500;color:PaleBlue"><?php echo $dbversion; ?></span>
+<span style = "font-size: 12px;letter-spacing:2px;font-family: &quot;sans-serif&quot;, sans-serif;font-weight:500;color:white">
+    <a href="https://github.com/DL3EL/SVXLink-Dash-V2">
+    <?php echo $dbversion; ?>
+    </a>
+</span>
 
 </p></center>
 </div></div>
