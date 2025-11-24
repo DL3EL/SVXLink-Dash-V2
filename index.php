25b7a0641ecacb11a4340bbee39c67550c1e1db5
root <dl3el@darc.de>
2025-11-24 22:20:33 +0100

    bugfix
    Signed-off-by: root <dl3el@darc.de>

--- index.php
+++ index.php
@@ -33,7 +33,6 @@ if (session_status() === PHP_SESSION_NONE) {
     include_once "include/browserdetect.php";
 //	$mobile = 1 = mobile;
   }
-	$mobile = 0;
   if ((defined ('debug')) && (debug > 0)) echo "<br>Browser: $mobile (0=Desktop, 1=MOBIL)<br>";
   if (!$mobile) {
     echo '<body>';
