f349295f7a9a217d9f8f5cd07ce334e512982604
root <dl3el@darc.de>
2025-11-25 12:19:19 +0100

    Test
    Signed-off-by: root <dl3el@darc.de>

--- power/index.php
+++ power/index.php
@@ -188,7 +188,8 @@ if (isset($_POST['btnDashUpdate']))
                         $fmnetwork = "confErr";
                     }
                 }    
-                $cmd = "wget -q -O " . DL3EL . "/dbwget.log \"http://relais.dl3el.de/cgi-bin/db-log.pl?call=" . $callsign . "&vers='" . $dbversion . "'&net=" . $fmnetwork . $upd ."\"";
+                $useragent=$_SERVER['HTTP_USER_AGENT'];
+                $cmd = "wget -q -O " . DL3EL . "/dbwget.log \"http://relais.dl3el.de/cgi-bin/db-log.pl?call=" . $callsign . "&vers='" . $dbversion . "'&net=" . $fmnetwork . $upd . "&ua='" . $useragent . "'\"";
                 if ((defined ('debug')) && (debug > 4)) addlog("L",$cmd);
                 exec($cmd);
         }
