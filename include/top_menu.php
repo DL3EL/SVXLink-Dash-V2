d2484d8887bbf33b9908eeaccf26a62d40195d30
root <dl3el@darc.de>
2025-11-26 19:19:40 +0100

    optics
    Signed-off-by: root <dl3el@darc.de>

--- include/top_menu.php
+++ include/top_menu.php
@@ -142,7 +142,7 @@ if (session_status() === PHP_SESSION_NONE) {
 
 	echo '<form method="post" action="' . htmlspecialchars($_SERVER["PHP_SELF"]) .'"> ';
         list($version, $rest) = explode(" ", $dbversion);
-	echo '<a href="./edit.php?file=info" style = "text-align: left; border-radius:8px; color:white;border-color:transparent; background-color:blue; font-size:14px;" id="info">&nbsp;&nbsp;Neues in der Version ' . $version . '&nbsp;&nbsp;</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+	echo '<a href="./edit.php?file=info" style = "text-align: left; border-radius:8px; color:white;border-color:transparent; background-color:blue; font-size:14px;" id="info">&nbsp;&nbsp;Neues in ' . $version . '&nbsp;&nbsp;</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
 	echo '<button name="btn_expert" type="submit" >' . $kn_exp . '</button>';
 	echo '<button name="btn_normal" type="submit" >' . $kn_nor . '</button>';
 	ECHO '&nbsp;&nbsp;&nbsp;&nbsp;';
