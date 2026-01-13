af51edd8a791620a69b12ba8ba1084318abe3074
root <dl3el@darc.de>
2026-01-13 14:55:18 +0100

    test
    Signed-off-by: root <dl3el@darc.de>

--- dl3el/aprs-is-msg.pl
+++ dl3el/aprs-is-msg.pl
@@ -134,7 +134,6 @@ my $exit_script = 0;
  	my $aprs_rxdatei = $dir  . "aprs-is.txt";
  	my $aprs_txdatei = $dir  . "aprs-tx.msg";
  	my $aprs_bcdatei = $dir  . "tg_status";
-	   $aprs_ok_datei
 	my $aprs_ok_datei = $dir  . "aprs-login.ok";
 	my $aprs_exit_datei = $dir  . "aprs.exit";
 	my $aprs_follow_pos = $dir  . "aprs-follow.pos";
