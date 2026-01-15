d37b211cf5699077dbe065925e1fc309e6f7f387
root <dl3el@darc.de>
2026-01-15 14:45:58 +0100

    test
    Signed-off-by: root <dl3el@darc.de>

--- relais.php
+++ relais.php
@@ -58,7 +58,7 @@ include_once "include/page_top.php";
         if ($last_pos === 2) {
             echo "Position von APRS_Follow (" . $position[3] . ", " . $position[2] . ") wird verwendet";
         } elseif ($last_pos === 3) {
-            echo "Position von APRS_Follow (" . $position[2] . ") wird verwendet";
+            echo "Position von APRS_Follow";
         } else {
             echo "Position der letzten Anfrage wird verwendet";
         }
