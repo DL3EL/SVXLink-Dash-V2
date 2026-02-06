a5a5208f34173a1aecb8177c10673279101678e2
root <dl3el@darc.de>
2026-02-06 12:24:42 +0100

    test
    Signed-off-by: root <dl3el@darc.de>

--- dl3el/DMRID_update.sh
+++ dl3el/DMRID_update.sh
@@ -5,9 +5,7 @@ DMRIDFILEok="$SCRIPT_DIR/"DMRIds.dat
 RADIOIDFILE="$SCRIPT_DIR/"radioid.dat
 cd "$SCRIPT_DIR/"
 echo "Downloading DMR database..."
-curl --fail -o ${RADIOIDFILE} 'https://database.radioid.net/static/user.csv' 
-echo "Converting usr.bin to DMRids.dat (saving space)"
-awk -F, '{print $1" "$2" "$3}' $RADIOIDFILE > $DMRIDFILE
+curl --fail -o ${DMRIDFILE} 'http://192.68.17.16/md380/UserInfo/user.fm' 
 chown svxlink:svxlink $DMRIDFILE 
 rm $DMRIDFILEok 
 mv $DMRIDFILE $DMRIDFILEok 
