79059f976cfd1722c831ae9ed56f783fab5141eb
root <dl3el@darc.de>
2026-02-08 18:48:44 +0100

    test
    Signed-off-by: root <dl3el@darc.de>

--- caller_svxmqtt.php
+++ caller_svxmqtt.php
@@ -4,6 +4,7 @@
         echo '<td valign="top" style = "border:none;">';
 //		echo '<object style="outline:none; width:500px; height:1412px; justify-content: left;" data=svx2mqtt/index_duo.php></object>';
 		echo '<object style="outline:none; width:500px; height:850px; justify-content: left;" data=svx2mqtt/index_duo.php></object>';
+        echo '<center><small><br>Live Date vom FM-Funknetz.de <br> basierend auf dem MQTT Livedashboard von DJ1JAY<br>(integriert von Frank, DL4EM)</small></center>';
         echo '</td>';
     }    
 ?>
