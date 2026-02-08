8e8fb70914ef73bb5e176c15c8f1fe0b0e02abd9
root <dl3el@darc.de>
2026-02-08 14:06:58 +0100

    test
    Signed-off-by: root <dl3el@darc.de>

--- caller_svxmqtt.php
+++ caller_svxmqtt.php
@@ -1,5 +1,5 @@
 <?php
-    if ((defined('DL3EL_LIVEDB_AUTO')) && (DL3EL_LIVEDB_AUTO === "yes")) {
+    if (((defined('DL3EL_LIVEDB_AUTO')) && (DL3EL_LIVEDB_AUTO === "yes")) || ((defined('DL3EL_LIVEDB')) && (DL3EL_LIVEDB === "yes"))) {
     // Live DB
         echo '<td valign="top" style = "border:none;">';
 //		echo '<object style="outline:none; width:500px; height:1412px; justify-content: left;" data=svx2mqtt/index_duo.php></object>';
