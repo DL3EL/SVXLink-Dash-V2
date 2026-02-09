d600f9a8dd223875ccd7c8e6dc6fff4f6288523d
root <dl3el@darc.de>
2026-02-09 15:53:42 +0100

    test
    Signed-off-by: root <dl3el@darc.de>

--- caller_svxmqtt.php
+++ caller_svxmqtt.php
@@ -1,10 +1,19 @@
 <?php
-    if (((defined('DL3EL_LIVEDB_AUTO')) && (DL3EL_LIVEDB_AUTO === "yes")) || ((defined('DL3EL_LIVEDB')) && (DL3EL_LIVEDB === "yes"))) {
+// DL3EL_LIVEDB === "right" LiveDB rechts
+// DL3EL_LIVEDB === "top" LiveDB über Menu oben
+// DL3EL_LIVEDB undiefined: LiveDB rechts
+
+	if ($_SESSION['auth'] !== "AUTHORISED") {
+        echo "not authorized<br>";
+	    return;
+    }
+    if ((!defined('DL3EL_LIVEDB')) || ((defined('DL3EL_LIVEDB')) && (DL3EL_LIVEDB === "right"))) {
     // Live DB
         echo '<td valign="top" style = "border:none;">';
 //		echo '<object style="outline:none; width:500px; height:1412px; justify-content: left;" data=svx2mqtt/index_duo.php></object>';
 		echo '<object style="outline:none; width:500px; height:850px; justify-content: left;" data=svx2mqtt/index_duo.php></object>';
         echo '<center><small>Live Daten vom FM-Funknetz.de<br>basierend auf dem MQTT Livedashboard von DJ1JAY<br>(integriert von Frank, DL4EM)</small></center>';
         echo '</td>';
-    }    
+    }   
+   
 ?>
