4f5536fdfd8d69554d4acccda7a52acdc23895e3
root <dl3el@darc.de>
2026-02-15 00:36:00 +0100

    bugfix
    Signed-off-by: root <dl3el@darc.de>

--- include/top_menu.php
+++ include/top_menu.php
@@ -68,11 +68,17 @@ if (session_status() === PHP_SESSION_NONE) {
 	if ((defined('DL3EL_DXCLUSTER')) && (DL3EL_DXCLUSTER === "yes")) {
 	    echo '<a href="./caller.php?id=dxcluster" style = "color: #0000ff;" target="_top">DX</a> | ';
 	}    
-	if ((!defined('DL3EL_OPENHAMCLOCK')) || (DL3EL_OPENHAMCLOCK !== "no")) {
+	if (!defined('DL3EL_OPENHAMCLOCK')) {
+	    $openhamclock = "https://openhamclock.com/";
+	} else {    
+	    $openhamclock = DL3EL_OPENHAMCLOCK;
+	}
+
+	if ($openhamclock !== "no") {
 	    if ((defined('DL3EL_OPENHAMCLOCK_TAB')) && (DL3EL_OPENHAMCLOCK_TAB === "new")) {
-		echo '<a href="' . DL3EL_OPENHAMCLOCK . '" style = "color: #0000ff;" target="OHC" rel="noopener noreferrer">OHC | </a> ';
+		echo '<a href="' . $openhamclock . '" style = "color: #0000ff;" target="OHC" rel="noopener noreferrer">OHC | </a> ';
 	    } else{
-		echo '<a href="./caller_extern.php?id=' . DL3EL_OPENHAMCLOCK . '" style = "color: #0000ff;">OHC | </a> ';
+		echo '<a href="./caller_extern.php?id=' . $openhamclock . '" style = "color: #0000ff;">OHC | </a> ';
 	    }    
 	}    
 	if (defined('DL3EL_OPENWEBRX')) {
